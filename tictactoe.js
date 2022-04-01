633e064fc1d402a720f282c04c42986a27d7b8a5
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-04-01 23:13:47 +0700

    Create HTML layout

---
+++ tictactoe.js
@@ -0,0 +1,21 @@
+const GameBoard = (function() {
+    let gameBoardArr = [];
+
+    return {
+        gameBoardArr,
+    }
+})();
+
+const DisplayController = (function() {
+    const displayBoard = (gameBoardArr) => {
+        
+    }
+
+    return {
+        displayBoard,
+    }
+})();
+
+const Player = () => {
+
+};
